88d8d857f57fb2a3f5be4ed1dcf47e47f7176c30
Patrick Hannan <patrick@hannan.com>
2020-10-07 02:11:08 -0400

    Added connection to mysql localhost.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 const inquirer = require("inquirer")
-const mysql = require("mysql")
+const mysql = require("mysql");
+const { start } = require("repl");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "employee_db",
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    start();
+  });
